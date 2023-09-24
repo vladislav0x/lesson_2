1f6a7515800a66a8a9c0a468c71a5baa842f8278
vladislavfirsenko <vlad.4834bc@mail.ru>
2023-09-24 18:28:56 +0300

    Добавил autouse=True в фикстуры

--- conftest.py
+++ conftest.py
@@ -2,16 +2,12 @@ import pytest
 from selene import browser
 
 
-@pytest.fixture()
+@pytest.fixture(autouse=True)
 def setting_browser():
     browser.config.window_width = 1280
     browser.config.window_height = 720
 
 
-
-
-@pytest.fixture()
+@pytest.fixture(autouse=True)
 def browser_open_google():
     browser.open('https://google.com')
-
-
--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -2,12 +2,12 @@ from selene.support.shared import browser
 from selene import be, have
 
 
-def test_search(setting_browser, browser_open_google):
+def test_search():
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser'))
 
 
-def test_search_second(setting_browser, browser_open_google):
+def test_search_second():
     browser.element('[name="q"]').should(be.blank).type('Шла1Саша2По3шоссе4и5сосала5сушку').press_enter()
     browser.element('[id="result-stats"]').should(have.text('Результатов: примерно 0'))
 
